01ab2bc54c5423bfcde62e9e3a784519fbdd85ec
JunYoung0131 <wnsdud5003@gmail.com>
2021-09-25 11:19:02 +0900

    mainpage

---
+++ FireJokProject/main/templates/script.js
@@ -0,0 +1,137 @@
+window.onload = function () {
+  const car1 = document.querySelector(".car1");
+  const car2 = document.querySelector(".car2");
+  const car3 = document.querySelector(".car3");
+  const car4 = document.querySelector(".car4");
+  const car5 = document.querySelector(".car5");
+  const car6 = document.querySelector(".car6");
+  const car7 = document.querySelector(".car7");
+  const car8 = document.querySelector(".car8");
+  const car9 = document.querySelector(".car9");
+
+  const button1 = document.querySelector(".button1");
+  const button2 = document.querySelector(".button2");
+  const button3 = document.querySelector(".button3");
+
+  car1.onclick = function () {
+    car1.style.background = "#ff470c";
+    car2.style.background = "white";
+    car3.style.background = "white";
+    car4.style.background = "white";
+    car5.style.background = "white";
+    car6.style.background = "white";
+    car7.style.background = "white";
+    car8.style.background = "white";
+    car9.style.background = "white";
+  };
+
+  car2.onclick = function () {
+    car1.style.background = "gray";
+    car2.style.background = "#ff470c";
+    car3.style.background = "white";
+    car4.style.background = "white";
+    car5.style.background = "white";
+    car6.style.background = "white";
+    car7.style.background = "white";
+    car8.style.background = "white";
+    car9.style.background = "white";
+  };
+
+  car3.onclick = function () {
+    car1.style.background = "gray";
+    car2.style.background = "white";
+    car3.style.background = "#ff470c";
+    car4.style.background = "white";
+    car5.style.background = "white";
+    car6.style.background = "white";
+    car7.style.background = "white";
+    car8.style.background = "white";
+    car9.style.background = "white";
+  };
+
+  car4.onclick = function () {
+    car1.style.background = "gray";
+    car2.style.background = "white";
+    car3.style.background = "white";
+    car4.style.background = "#ff470c";
+    car5.style.background = "white";
+    car6.style.background = "white";
+    car7.style.background = "white";
+    car8.style.background = "white";
+    car9.style.background = "white";
+  };
+
+  car5.onclick = function () {
+    car1.style.background = "gray";
+    car2.style.background = "white";
+    car3.style.background = "white";
+    car4.style.background = "white";
+    car5.style.background = "#ff470c";
+    car6.style.background = "white";
+    car7.style.background = "white";
+    car8.style.background = "white";
+    car9.style.background = "white";
+  };
+  car6.onclick = function () {
+    car1.style.background = "gray";
+    car2.style.background = "white";
+    car3.style.background = "white";
+    car4.style.background = "white";
+    car5.style.background = "white";
+    car6.style.background = "#ff470c";
+    car7.style.background = "white";
+    car8.style.background = "white";
+    car9.style.background = "white";
+  };
+  car7.onclick = function () {
+    car1.style.background = "gray";
+    car2.style.background = "white";
+    car3.style.background = "white";
+    car4.style.background = "white";
+    car5.style.background = "white";
+    car6.style.background = "white";
+    car7.style.background = "#ff470c";
+    car8.style.background = "white";
+    car9.style.background = "white";
+  };
+  car8.onclick = function () {
+    car1.style.background = "gray";
+    car2.style.background = "white";
+    car3.style.background = "white";
+    car4.style.background = "white";
+    car5.style.background = "white";
+    car6.style.background = "white";
+    car7.style.background = "white";
+    car8.style.background = "#ff470c";
+    car9.style.background = "white";
+  };
+  car9.onclick = function () {
+    car1.style.background = "gray";
+    car2.style.background = "white";
+    car3.style.background = "white";
+    car4.style.background = "white";
+    car5.style.background = "white";
+    car6.style.background = "white";
+    car7.style.background = "white";
+    car8.style.background = "white";
+    car9.style.background = "#ff470c";
+  };
+
+  button1.onclick = function () {
+    button1.style.background = "#ff470c";
+    button2.style.background = "gray";
+    button3.style.background = "gray";
+  };
+
+  button2.onclick = function () {
+    button1.style.background = "gray";
+    button2.style.background = "#ff470c";
+    button3.style.background = "gray";
+  };
+
+  button3.onclick = function () {
+    button1.style.background = "gray";
+    button2.style.background = "gray";
+    button3.style.background = "#ff470c";
+  };
+};
